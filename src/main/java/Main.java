36324c5b0fa4002cc440751646e3ffc0654a0a43
Jakub Kvapil <kvapiljakub5@gmail.com>
2025-11-28 13:41:39 +0100

    First commit with a few functions already available

---
+++ src/main/java/Main.java
@@ -0,0 +1,56 @@
+import Utilities.Util;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import static Utilities.Util.*;
+
+public class Main {
+    private static final BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(System.in));
+    private static Boolean running = true;
+    private static File pwd;
+
+    public static void main(String[] args) throws IOException {
+
+        while (running) {
+            pwd = Util.pwd;
+            newLine();
+
+            String[] input = reader.readLine().split(" ");
+            switch (input[0]) {
+                case "man", "help":
+                    printMan();
+                    break;
+                case "exit":
+                    running = false;
+                    break;
+                case "return":
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for (int i = 1; i < input.length; i++) {
+                        stringBuilder.append(input[i] + " ");
+                    }
+                    System.out.println(stringBuilder.toString());
+                case "cd":
+                    if (!(input.length > 2)) {
+                        if(input[1].contains("-r")){
+                            cd(true, input[1]);
+                        }
+                        else{cd(input[1]);}
+                    }
+                    else {System.out.println("Too many arguments!");}
+                    break;
+                case "ls":
+                    listDirectories(pwd);
+                    //lists files in current directory
+                    break;
+                case "cat":
+                    break;
+                case "pwd":
+                    System.out.println(pwd.getAbsolutePath());
+                    break;
+                default:
+                    System.out.println("Unknown command");
+                    break;
+            }
+        }
+    }
+}
---
+++ src/main/java/Utilities/Util.java
@@ -0,0 +1,88 @@
+package Utilities;
+import java.io.File;
+import java.util.Objects;
+
+public class Util {
+    public static String userDir = System.getProperty("user.home").toString();
+    public static File pwd = new File(userDir);
+
+    //man
+    public static void printMan() {
+        System.out.println("Manual:");
+        System.out.println("cd [path] - change directory to [path]");
+        System.out.println("cd [-flag] - change directory using flag");
+        System.out.println("    [-r] - return to root directory");
+        System.out.println("ls - list directories in current directory");
+        System.out.println("pwd - print working directory");
+        System.out.println("exit - exit the program");
+    }
+
+    //CD
+    public static void cd(String path) {
+        if (path.equals("..")) {
+            try {
+                pwd = pwd.getParentFile();
+                return;
+            }catch (Exception e){
+                System.out.println("There is no directory above this one!"); //TODO wth...
+            }
+        }
+
+        String dummyPWD = pwd.toString() + "/" + path;
+        try{
+            File testPWD = new File(dummyPWD);
+            if(testPWD.isDirectory()){
+                pwd = testPWD;
+            }
+        }
+        catch(Exception e){System.out.println("No such directory");}
+    }
+    public static void cd(Boolean yes, String flag) {
+        if(Objects.equals(flag, "-r")){
+            pwd = new File("C:\\Users\\Jakub");
+            return;
+        }
+    }
+
+    //ls
+    public static void listDirectories(File directory) {
+        File[] files = directory.listFiles();
+        int count = 0;
+
+//        if(files.length > 25){
+//            System.out.println("Do you want to continue? (y/n)");
+//        }
+
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    System.out.println(file.getName());
+                }
+            }
+        }
+        else{
+            System.out.println("No files found!");
+        }
+    }
+
+    //cat
+    public static void cat(String filename) {
+        //TODO implement cat
+        File[] files = pwd.listFiles();
+        assert files != null;
+        for (File file : files) {
+            if (file.getName().equals(filename)) {
+                System.out.println("File found");
+            }
+        }
+    }
+
+    public static void newLine(){
+        String name = System.getProperty("user.name") + "@" + System.getenv("COMPUTERNAME");
+        //TODO write something like user@pc:~/current/directory$
+        if(pwd.toString().equals(System.getProperty("user.home").toString())){
+            System.out.print(name + " ~" + "\n" + "> ");
+        }
+        else{System.out.print(name + " " + pwd.getAbsolutePath() + "\n" + "> ");}
+    }
+}
